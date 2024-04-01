c38106171180a0a8539c847d770bee8abffce6b5
keane <keaneathallah960@gmail.com>
2024-04-01 14:39:39 +0800

    Creating user api for manage restaurant

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -23,3 +23,27 @@ export const validateMyUserRequest = [
   body("country").isString().notEmpty().withMessage("Country must be a string"),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City name is required"),
+  body("country").notEmpty().withMessage("Country name is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery Price must be a postive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated Delivery time must be a positive interger"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array must not empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be positive number"),
+  handleValidationErrors,
+];
